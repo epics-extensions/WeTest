a791f8a12519fe6321509b555c838a99722a075b
Minijackson <minijackson@riseup.net>
2023-01-23 14:24:02 +0100

    update Nix and Poetry dependencies

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
   outputs = { self, nixpkgs }: let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
@@ -9,6 +9,12 @@
 
     packages.x86_64-linux.default = pkgs.poetry2nix.mkPoetryApplication {
       projectDir = ./.;
+
+      # Numpy 1.19.5 doesn't compile with Python3.10+
+      #
+      # This was fixed in later versions of Numpy, but Numpy >=1.20 doesn't
+      # support Python 3.6, which we need to support older systems.
+      python = pkgs.python39;
 
       propagatedBuildInputs = with pkgs.python3Packages; [ tkinter ];
 
