b8f713fdf9d6aa9c6f964dcffbb33d8ec6206736
Minijackson <minijackson@riseup.net>
2023-11-27 13:23:35 +0100

    chore: support more architectures with Nix, add a simple test

--- flake.nix
+++ flake.nix
@@ -1,33 +1,61 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
-
-  outputs = { self, nixpkgs }: let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-
-    packages.x86_64-linux.default = pkgs.poetry2nix.mkPoetryApplication {
-      projectDir = ./.;
-
-      # Numpy 1.19.5 doesn't compile with Python3.10+
-      #
-      # This was fixed in later versions of Numpy, but Numpy >=1.20 doesn't
-      # support Python 3.6, which we need to support older systems.
-      python = pkgs.python39;
-
-      propagatedBuildInputs = with pkgs.python39Packages; [ tkinter ];
-
-      overrides = pkgs.poetry2nix.overrides.withDefaults (final: prev: {
-        reportlab = prev.reportlab.overridePythonAttrs (old: {
-          buildInputs = with pkgs; [ (freetype.overrideAttrs (_: { dontDisableStatic = true; })) ];
-        });
-      });
-    };
-
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; [ poetry python39Full ];
-    };
-
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    epnix.url = "github:epics-extensions/EPNix";
   };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    epnix,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          epnix.overlays.default
+          (_final: _prev: {
+            wetest = self.packages.${system}.default;
+          })
+        ];
+      };
+    in {
+      packages.default = pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = ./.;
+
+        # Numpy 1.19.5 doesn't compile with Python3.10+
+        #
+        # This was fixed in later versions of Numpy, but Numpy >=1.20 doesn't
+        # support Python 3.6, which we need to support older systems.
+        python = pkgs.python39;
+
+        nativeBuildInputs = with pkgs; [makeWrapper];
+        propagatedBuildInputs = with pkgs.python39Packages; [tkinter];
+
+        overrides = pkgs.poetry2nix.overrides.withDefaults (_final: prev: {
+          reportlab = prev.reportlab.overridePythonAttrs (_old: {
+            buildInputs = with pkgs; [(freetype.overrideAttrs (_: {dontDisableStatic = true;}))];
+          });
+        });
+
+        postInstall = let
+          inherit (epnix.packages.x86_64-linux) epics-base;
+        in ''
+          wrapProgram $out/bin/wetest \
+            --set PYEPICS_LIBCA "${epics-base}/lib/linux-x86_64/libca.so"
+        '';
+
+        doCheck = true;
+      };
+
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [poetry python39Full];
+      };
+
+      checks = import ./nix/checks {inherit pkgs;};
+    });
 }
---
+++ nix/checks/default.nix
@@ -0,0 +1,3 @@
+args: {
+  simple = import ./simple.nix args;
+}
