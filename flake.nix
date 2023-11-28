5ca3ba8bb664f46838e4055d4de19909472fbe67
Minijackson <minijackson@riseup.net>
2023-11-28 11:35:58 +0100

    chore: add pytest, bump min Python version to 3.7

--- flake.nix
+++ flake.nix
@@ -24,33 +24,42 @@
         ];
       };
     in {
-      packages.default = pkgs.poetry2nix.mkPoetryApplication {
-        projectDir = ./.;
-
+      packages.default = let
         # Numpy 1.19.5 doesn't compile with Python3.10+
         #
         # This was fixed in later versions of Numpy, but Numpy >=1.20 doesn't
         # support Python 3.6, which we need to support older systems.
         python = pkgs.python39;
+      in
+        pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
 
-        nativeBuildInputs = with pkgs; [makeWrapper];
-        propagatedBuildInputs = with pkgs.python39Packages; [tkinter];
+          inherit python;
 
-        overrides = pkgs.poetry2nix.overrides.withDefaults (_final: prev: {
-          reportlab = prev.reportlab.overridePythonAttrs (_old: {
-            buildInputs = with pkgs; [(freetype.overrideAttrs (_: {dontDisableStatic = true;}))];
+          nativeBuildInputs = with pkgs; [makeWrapper];
+          propagatedBuildInputs = with python.pkgs; [tkinter];
+          nativeCheckInputs = with python.pkgs; [pytestCheckHook];
+
+          overrides = pkgs.poetry2nix.overrides.withDefaults (_final: prev: {
+            reportlab = prev.reportlab.overridePythonAttrs (_old: {
+              buildInputs = with pkgs; [(freetype.overrideAttrs (_: {dontDisableStatic = true;}))];
+            });
           });
-        });
 
-        postInstall = let
-          inherit (epnix.packages.x86_64-linux) epics-base;
-        in ''
-          wrapProgram $out/bin/wetest \
-            --set PYEPICS_LIBCA "${epics-base}/lib/linux-x86_64/libca.so"
-        '';
+          postInstall = let
+            inherit (epnix.packages.x86_64-linux) epics-base;
+          in ''
+            wrapProgram $out/bin/wetest \
+              --set PYEPICS_LIBCA "${epics-base}/lib/linux-x86_64/libca.so"
+          '';
 
-        doCheck = true;
-      };
+          doCheck = false;
+          checkPhase = ''
+            runHook preCheck
+            pytest
+            runHook postCheck
+          '';
+        };
 
       devShells.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [poetry python39Full];
